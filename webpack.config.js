a9e5569d419c21e627218dcb798fe87c4c5e7c93
linlichun <linlichun@meicai.cn>
2019-11-10 19:09:19 +0800

    coding

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8000,
+    hot: true,
+    overlay: true,
+    proxy: {
+      '/api': {
+        target: 'localhost:3000',
+        changeOrigin: true,
+        logLevel: 'debug',
+      }
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'react-web',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true
+      },
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        use: [
+          'url-loader'
+        ]
+      }
+    ]
+  }
+}
